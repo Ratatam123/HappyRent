0af1ca4b11f506adeca022bad0750d2e597a9084
Lorenz Hupfeld <lorenz.hupfeld@mailbox.org>
2019-06-25 12:48:32 +0200

    turned off debug mode

--- property_project/__init__.py
+++ property_project/__init__.py
@@ -2,13 +2,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from property_project.config import Config
-#from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object(Config)  # load OAUTH keys etc. into the app
-app.debug = True
-
-#toolbar = DebugToolbarExtension(app)
 
 db = SQLAlchemy(app)  # create engine etc. is created behind the scenes
 
