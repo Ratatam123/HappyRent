ef6fc9527b6c14733e40abaf2b53746848037db9
Lorenz Hupfeld <lorenz.hupfeld@mailbox.org>
2019-08-04 14:42:50 +0200

    Added config.json, adapted config.json, updated routes.py (home-route)

--- property_project/config.py
+++ property_project/config.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
-import os
 
+import os
+import json
+
+with open('/config.json') as config_file:
+        config = json.load(config_file)
 
 class Config:
     # os.environ.get('SECRET_KEY')
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SECRET_KEY = config.get('SECRET_KEY')
 
     CACHE_TYPE = "null"
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///properties.db'
-
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://happy_renter:best_rents@localhost/happyrent_db'
+    
     # GOOGLE OAUTH
     # os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
-    GOOGLE_OAUTH_CLIENT_ID = '646163642970-koimt79vmr3355q1sldsd9e68hvvr336' +\
-        '.apps.googleusercontent.com'
+    GOOGLE_OAUTH_CLIENT_ID = config.get('GOOGLE_OAUTH_CLIENT_ID')
     # os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET')
-    GOOGLE_OAUTH_CLIENT_SECRET = 'asUsmHtCv1TsfWa-gsJBJoOd'
+    GOOGLE_OAUTH_CLIENT_SECRET = config.get('GOOGLE_OAUTH_CLIENT_SECRET')
 
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     OAUTHLIB_RELAX_TOKEN_SCOPE = True
-
-
 
 
 
